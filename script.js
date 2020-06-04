ec2cde952b9f3cbb74715120e338d7168555bf9b
daat2 <ibi.olu@gmail.com>
2020-06-04 12:25:07 +0000

    basic operator

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log('hello world!')
+var firstName = 'joh';
+console.log(firstName);
+
+var fullAge= true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+job ="accounting";
+console.log(job);
+
+job = "pharmacy";
+console.log(job);
+
+
+var age= 22;
+console.log(firstName +'',  + age )
+
+var job, isMarried,teacher
+console.log(isMarried)
+
+job = "taylor";
+console.log(job);
+job = "isMarried";
+console.log(job);
+job = "teacher";
+console.log(job);
+
+var job, isSpinster;
+ job = 'bookKeeper';
+ isSpinster = false;
+  console.log(isSpinster);
+  console.log(job);
+
+  console.log(firstName +  'is' +  ' a ' + age +'year old' + job +'.is he spinster?' + isSpinster);
+//    var lastName = prompt('what is his last name?');
+//    console.log(firstName + '' + lastName);
+
+   var year, yearJohn;
+   year = 2020;
+   yearJohn = year -28
+   console.log(yearJohn);
